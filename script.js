4f9bb0acda701dec50a5767a7719dbd8d0d91116
barbig27 <tungbarbig@gmail.com>
2023-07-25 20:24:57 +0700

    them huong dan dung mod skin

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById("openModal").addEventListener("click", function() {
+    var modal = document.getElementById("modal");
+    modal.style.display = "block";
+  });
+  
+  document.getElementById("closeModal").addEventListener("click", function() {
+    var modal = document.getElementById("modal");
+    modal.style.display = "none";
+  });
+  
